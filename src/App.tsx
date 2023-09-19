3fbc7aeebfcbcb78081602b5c7d8019b54a21de6
mehdiap99 <s.mehdi.alipour.01999@gmail.com>
2023-09-19 16:21:41 +0330

    project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import './components/auth/login/login';
+import Login from './components/auth/login/login';
+
+function App() {
+
+  return (
+    <>
+
+    <Login></Login>
+    </>
+  )
+}
+
+export default App
