32f9160e956e2baad209ed090f1ae7fda558b3ee
YasinSahebi <yaaasinsahebi@gmail.com>
2023-09-22 13:11:37 -0800

    ButtonComponent Repeir

---
+++ src/components/common/button/index.tsx
@@ -0,0 +1,15 @@
+// <======== Code-Files ========> //
+import '../../../index.css'; 
+
+
+interface IButtonprops extends React.PropsWithChildren {
+    ClassName: string;
+}
+
+const Button: React.FC<IButtonprops> = ({ClassName, children}): JSX.Element => {
+    return (
+        <button className={`${ClassName}`}>{children}</button>
+    )
+};
+
+export default Button;
