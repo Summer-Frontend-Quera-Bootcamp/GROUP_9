b2db11e1173ebcb6b639c1c9d4d8ad8f94474b36
YasinSahebi <yaaasinsahebi@gmail.com>
2023-09-22 13:13:07 -0800

    LinkComponent Repair

---
+++ src/components/common/LinkButton/index.tsx
@@ -0,0 +1,16 @@
+// <======== Code-Files ========> //
+import '../../../index.css'; 
+
+
+interface ILinkButtonprops extends React.PropsWithChildren {
+    ClassName : string;
+    href : string;
+}
+
+const Link: React.FC<ILinkButtonprops> = ({ClassName, href, children}): JSX.Element => {
+    return (
+        <a href={`${href}`} className={`${ClassName}`}>{children}</a>
+    )
+};
+
+export default Link;
