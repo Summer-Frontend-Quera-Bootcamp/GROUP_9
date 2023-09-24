187237a4beb5733a4ee79de7f3298589e9a0772e
Abolfazl Mikaniki <abolfazlmikaniki6@gmail.com>
2023-09-24 12:47:40 +0330

    create sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 // <======== Code-Files ========> //
 import "./index.css";
 
+
 // <======== Component-Files ========> //
 // import LogInPage from "./pages/Authentication Pages/Login Page";
 import ListView from "./pages/Main Pages/Board Pages/ListView";
-
+import Sidebar from "./components/Sidebar";
+import AuthenticationLayout from "./pages/Authentication Pages/Layout";
 // <======== Hook ========> //
 
 const App = () => {
   return (
     <>
+      
       {/* <LogInPage /> */}
+      <Sidebar></Sidebar>
       <ListView />
     </>
   );
