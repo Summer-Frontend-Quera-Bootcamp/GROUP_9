912938de47dd7da3e27ba8845197e8a6070e579a
AlirezaNovin <142053893+AlirezaNovin@users.noreply.github.com>
2023-09-26 00:26:31 +0330

    alireza2 branch has been updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@
 import "./index.css";
 
 // <======== Component-Files ========> //
-import MainLayout from "./pages/Main Pages/Layout";
 import BoardView from "./pages/Main Pages/Board Pages/BoardView/index";
+import ListView from "./pages/Main Pages/Board Pages/ListView/index";
 // <======== Hook ========> //
 
 
 const App = () => {
   return (
     <>
-    <MainLayout page="BoardView"></MainLayout>
     <BoardView/>
+    <ListView/>
     </>
     
     
