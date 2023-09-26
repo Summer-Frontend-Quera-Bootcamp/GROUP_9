acbb7b11ac33982330d68557a605e745851b655b
AlirezaNovin <142053893+AlirezaNovin@users.noreply.github.com>
2023-09-26 11:57:12 +0330

    ListView is done

--- src/pages/Main Pages/Board Pages/ListView/index.tsx
+++ src/pages/Main Pages/Board Pages/ListView/index.tsx
@@ -1,12 +1,12 @@
 // <======== Code-Files ========> //
-import Arrow from "../../../assets/Icon/Arrow.svg";
+import Arrow from "../../../assets/icone/Arrow.svg";
 // <======== Component-Files ========> //
 import MainLayout from "../../Layout/index";
 
 const ListView = () => {
   return (
     <MainLayout page="ListView">
-      <details className="w-full h-full mr-[364px] mt-[28px] overflow-x-scroll overflow-y-hidden">
+      <details className="w-full h-full mr-[364px] mt-[28px] overflow-y-scroll overflow-x-hidden">
         <summary className=" hug top-[198px] left-[1008px] w-[76px] h-[32px] gap-[8px] flex items-center">
           <img src={Arrow} />
           <p className="text-black font-IranYekan800 text-HeadingXS leading-8 marker:content-[''] cursor-pointer">
