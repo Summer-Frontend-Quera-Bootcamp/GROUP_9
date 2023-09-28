a4f56333d956e88170aa5018d72700f74c051dc1
Mohammad Sadegh <6msadegh@gmail.com>
2023-09-28 12:47:15 +0330

    Add & Complete ModalButton

---
+++ src/components/Modal/ModalButton/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+interface modalButtonProps{
+    title:string;
+    buttonClick:() => void;
+}
+
+const NewProjectModal: React.FC<modalButtonProps> = ({ title,buttonClick}) => {
+
+    return(
+        <button className={"flex justify-center w-[415px] h-[40px] rounded-[6px] p-[10px] bg-Brand-Primary"} onClick={buttonClick}>
+            <div className={"font-IranYekan800 text-BoldS text-White text-right"}>
+                {title}
+            </div>
+        </button>
+    )
+}
+export default NewProjectModal
