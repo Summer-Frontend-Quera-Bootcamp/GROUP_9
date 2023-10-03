f886d53d02d793367761630bdb2d8093fc62a806
Mohammad Sadegh <6msadegh@gmail.com>
2023-10-03 18:49:33 +0330

    Add & Complete InfoModal

---
+++ src/components/TaskManager/Modals/Create/Workspace/InfoModal/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Modal from "../../../../../common/Modal";
+
+interface modalProps{
+    isOpen: boolean;
+    onClose: ()  => void;
+    workSpaceName:string;
+    color:string;
+    buttonClick:() => void;
+    handleBack: () => void;
+}
+
+const InfoModal: React.FC<modalProps> = ({ isOpen, onClose, workSpaceName,color, handleBack, buttonClick }) => {
+    if (!isOpen) return null;
+
+    return (
+        <Modal activeModal={3} title={"مرور اطلاعات "} hasButton={true} onClose={onClose} isOpen={isOpen} isSlide={true} width={"501"} height={"369"} backButton={true} buttonTitle={"ساختن ورک‌اسپیس"} handleBack={handleBack} buttonClick={buttonClick}>
+            <div className={"flex flex-col font-IranYekan800 BoldS w-[453px] h-[169px] flex flex-row gap-s border-[0.5px] border-[#AAAAAA] py-[16px] px-[12px] rounded-[8px] gap-s"}>
+                <div className={"w-[429px] h-[34px] flex flex-row justify-between"}>
+                    <div> نام ورک‌اسپیس </div>
+                    <div> {workSpaceName} </div>
+                </div>
+                <div className={"w-[429px] h-[34px] flex flex-row justify-between"}>
+                    <div> رنگ ورک‌اسپیس </div>
+                    <div className={`w-[15px] h-[15px] rounded-[2px] bg-${color}-Primary`}> </div>
+                </div>
+                <div className={"w-[429px] h-[34px] flex flex-row justify-between"}>
+                    <div> اعضا </div>
+                    <div className={"w-[36px] h-[37px] rounded-[100px] bg-Red-Primary"}> </div>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+export default InfoModal
