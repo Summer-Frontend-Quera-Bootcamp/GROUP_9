65b3e59b8eaa2d8cb22f9b279971c215b2d265e8
YasinSahebi <yaaasinsahebi@gmail.com>
2023-10-19 06:38:36 -0800

    Create Toast for Authentication-Page

--- src/layouts/Authentication/index.tsx
+++ src/layouts/Authentication/index.tsx
@@ -3,9 +3,9 @@ import { BackGroundImage } from "../../assets/Icons/Authentication/BackGround";
 
 // <======== Component-Import ========> //
 import LoginOrSignupButton from "../../components/Athentication/LoginOrSignupButton";
+import AuthenticationToast from "./AuthenticationToast";
 
 // <======== Hooks ========> //
-
 
 interface TAuthenticationProps extends React.PropsWithChildren {
   page: "login" | "register" | "resetpassword" | "forgetpassword" | "success";
@@ -16,26 +16,29 @@ const Authentication: React.FC<TAuthenticationProps> = ({
   children,
 }): JSX.Element => {
   return (
-    <main className="w-full h-full pt-[80px] pb-0 px-[80px] flex flex-col justify-start items-center">
-      <header className="w-full max-w-[1280px] fixed top-[80px] flex flex-row justify-between items-center">
-        <h1 className="font-IranYekan800 text-HeadingL">کوئرا تسک منیجر</h1>
-        {page === "login" ? (
-          <LoginOrSignupButton
-            textContent="ثبت‌نام نکرده‌ای؟"
-            buttonContent="ثبت‌نام"
-          />
-        ) : (
-          <LoginOrSignupButton
-            textContent="قبلا ثبت‌نام کرده‌ای؟"
-            buttonContent="ورود"
-          />
-        )}
-      </header>
-      <main className="w-full h-full mt-[55px] flex justify-center items-center">
-        {children}
+    <>
+      <AuthenticationToast />
+      <main className="w-full h-full pt-[80px] pb-0 px-[80px] flex flex-col justify-start items-center">
+        <header className="w-full max-w-[1280px] fixed top-[80px] flex flex-row justify-between items-center">
+          <h1 className="font-IranYekan800 text-HeadingL">کوئرا تسک منیجر</h1>
+          {page === "login" ? (
+            <LoginOrSignupButton
+              textContent="ثبت‌نام نکرده‌ای؟"
+              buttonContent="ثبت‌نام"
+            />
+          ) : (
+            <LoginOrSignupButton
+              textContent="قبلا ثبت‌نام کرده‌ای؟"
+              buttonContent="ورود"
+            />
+          )}
+        </header>
+        <main className="w-full h-full mt-[55px] flex justify-center items-center">
+          {children}
+        </main>
+        {BackGroundImage}
       </main>
-      {BackGroundImage}
-    </main>
+    </>
   );
 };
 
