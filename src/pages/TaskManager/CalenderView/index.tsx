7431dddbae57e12e8fb3f4ade6f286006ab3bcee
mehdiap99 <s.mehdi.alipour.01999@gmail.com>
2023-10-22 15:24:17 +0330

    calenderVeiw ui

--- src/pages/TaskManager/CalenderView/index.tsx
+++ src/pages/TaskManager/CalenderView/index.tsx
@@ -1,17 +1,20 @@
 // <======== Assest-Import ========> //
 
 // <======== Component-Import ========> //
+import CalenderView from "../../../components/TaskManager/CalenderView";
 import Navbar from "../../../components/TaskManager/Navbar";
+import NewTaskBtn from "../../../components/TaskManager/NewTaskButton";
 
 // <======== Hooks ========> //
 
-
 const CalenderViewPage: React.FC = (): JSX.Element => {
-    return (
-        <>
-            <Navbar activePage="CalenderView"/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar activePage="CalenderView" />
+      <NewTaskBtn />
+      <CalenderView></CalenderView>
+    </>
+  );
+};
 
 export default CalenderViewPage;
